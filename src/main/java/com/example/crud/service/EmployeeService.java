9d6ebc820e42b07654e391544ab6d22f0a960f7e
Mihika Parmar <80045642+mihikaparmar@users.noreply.github.com>
2024-09-18 18:37:40 +0530

    first commit

---
+++ src/main/java/com/example/crud/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.crud.service;
+
+import java.util.List;
+
+import com.example.crud.entity.Employee;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> fetchAllEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployeeById(Long id, Employee employee);
+
+    String deleteDepartmentById(Long id);
+}
